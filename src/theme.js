83effc172030fff26411617dd0ed05561b32f89e
Thiago Carrilho Rodriguez <tcr@discente.ifpe.edu.br>
2020-08-21 23:21:16 -0300

    first commit

---
+++ src/theme.js
@@ -0,0 +1,11 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#e23e57',
+    },
+    secondary: { main: '#522546' },
+  },
+});
+
+export default theme;
