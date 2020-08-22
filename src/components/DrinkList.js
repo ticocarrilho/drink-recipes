e6f4de4c225a95a390bcffe0cb4cca87243be1ca
Thiago Carrilho Rodriguez <tcr@discente.ifpe.edu.br>
2020-08-22 14:30:36 -0300

    Added Redux

--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -1,27 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { Grid } from '@material-ui/core';
-import api from '../services/api'
+import React, { useEffect, useCallback } from 'react';
+import { Grid, CircularProgress } from '@material-ui/core';
 import DrinkListItem from './DrinkListItem';
+import { useSelector, useDispatch } from 'react-redux';
+import { drinksSelector, fetchDrinks } from '../slices/drinksSlice';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  loadingIcon: {
+    display: 'flex',
+    alignItems: 'center',
+    marginLeft: theme.spacing(3),
+  },
+}));
+
+const LoadingMessage = ({ classIcon }) => {
+  return (
+    <>
+      <h1>Loading...</h1>
+      <div className={classIcon}>
+        <CircularProgress size='2rem' />
+      </div>
+    </>
+  );
+};
 
 const renderDrinkList = (drinks) => {
-    return drinks.map((drink, i) => <DrinkListItem key={i} drink={drink}/>)
-}
+  return drinks.map((drink, i) => <DrinkListItem key={i} drink={drink} />);
+};
 
 function DrinkList() {
-    const [drinks, setDrinks] = useState([])
-    useEffect(() => {
-        const getDrinks = async () => {
-            const result = await api.get('/filter.php', { params: { a: 'Alcoholic' }})
-            setDrinks(result.data.drinks);
-            console.log(result.data.drinks)
-        }
-        getDrinks();
-        console.log(drinks)
-    }, [])
+  const classes = useStyles();
+  const dispatch = useDispatch();
+  const { drinks, loading } = useSelector(drinksSelector);
+
+  const fetchDrinksCallback = useCallback(() => {
+    dispatch(fetchDrinks());
+  }, [dispatch]);
+
+  useEffect(() => {
+    fetchDrinksCallback();
+  }, [fetchDrinksCallback]);
 
   return (
     <Grid container spacing={3}>
-    {renderDrinkList(drinks)}
+      {loading ? (
+        <LoadingMessage classIcon={classes.loadingIcon} />
+      ) : (
+        renderDrinkList(drinks)
+      )}
     </Grid>
   );
 }
