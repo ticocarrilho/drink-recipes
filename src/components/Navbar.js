c65c91746b3e06ba19f5be0664909d38ad68c761
Thiago Carrilho Rodriguez <tcr@discente.ifpe.edu.br>
2020-08-22 11:27:23 -0300

    Appbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,87 @@
+import React from 'react';
+import { Toolbar, AppBar, IconButton, Typography, InputBase } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@material-ui/icons/Search';
+import { fade, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    width: '100%',
+    marginLeft: 0,
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    }
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit'
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '18ch',
+      '&:focus': {
+        width: '25ch',
+      },
+    },
+  }
+}));
+
+function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <AppBar>
+      <Toolbar>
+        <IconButton className={classes.menuButton} edge='start' color='inherit'>
+          <MenuIcon />
+        </IconButton>
+        <Typography className={classes.title} variant='h6' noWrap>
+          Drink API
+        </Typography>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <SearchIcon />
+          </div>
+          <InputBase
+              placeholder="Search Drink…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+            />
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
---
+++ src/pages/MainPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from '@material-ui/core';
+import Navbar from '../components/Navbar';
+
+function MainPage() {
+  return (
+    <>
+      <Navbar />
+      <Container></Container>
+    </>
+  );
+}
+
+export default MainPage;
